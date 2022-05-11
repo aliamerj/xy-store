810575a50d1f2efe19d3e23546016260a633941e
joeamerson <aliamer19ali@gmail.com>
2022-05-11 13:54:13 +0300

    test

---
+++ server/testing/user/user.unit.test.js
@@ -0,0 +1,11 @@
+const User = require('../../modules/user.module');
+const { getUser } = require('../../routes/user/user.controller');
+
+// controller
+describe('controller.getUser', ()=>{
+    it('should return user in given id', ()=>{
+        User.findById('123') = jest.fn().mockReturnValue({_id: '123' , username : 'ali'})
+
+        expect(getUser).toBe({_id: '123' , username : 'ali'})
+    })
+})
