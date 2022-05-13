dfcb255bef373f6ba02c33438381f4c35c524451
joeamerson <aliamer19ali@gmail.com>
2022-05-13 10:30:59 +0300

    test validaters

--- server/testing/product/product.test.js
+++ server/testing/product/product.test.js
@@ -3,7 +3,6 @@ const request = require("supertest");
 const Product = require("../../modules/product.module");
 let server;
 let productTest;
-let productTestWitoutDate;
 jest.setTimeout(9000);
 
 beforeEach(() => {
@@ -23,8 +22,8 @@ afterEach(async () => {
   server.close();
 });
 
-describe("/api/product", () => {
-  describe("GET: getAllProduct /", () => {
+describe.skip("/api/product", () => {
+  describe("GET/ getAllProduct /", () => {
     it("should return all products", async () => {
       await Product.collection.insertMany([
         productTest,
@@ -38,7 +37,9 @@ describe("/api/product", () => {
           price: 2,
         },
       ]);
+
       const res = await request(server).get("/api/product");
+
       expect(res.status).toBe(200);
       expect(res.body.length).toBe(2);
       expect(
@@ -55,6 +56,7 @@ describe("/api/product", () => {
         )
       ).toBeTruthy();
     });
+
     it("should throw an error if there is problem", () => {
       request(server)
         .get("/api/product")
@@ -63,7 +65,7 @@ describe("/api/product", () => {
         .expect(404);
     });
   });
-  describe("GET:: getProduct ", () => {
+  describe("GET/ getProduct ", () => {
     it("should return product with given id  (200)", async () => {
       const product = new Product(productTest);
       await product.save();
@@ -76,7 +78,7 @@ describe("/api/product", () => {
       await request(server).get("/api/product/1").expect(404);
     });
   });
-  describe("POST / createProduct", () => {
+  describe("POST/ createProduct", () => {
     it("should add new product with 201 created", async () => {
       await request(server).post("/api/product").send(productTest).expect(201);
     });
@@ -87,55 +89,55 @@ describe("/api/product", () => {
         .expect(422);
     });
   });
-});
-describe("PUT / updateProduct", () => {
-  it("should update info with given valid id", async () => {
-    const product = new Product(productTest);
-    await product.save();
-    const res = await request(server)
-      .put("/api/product/" + product._id)
-      .send({
-        title: "changed changed changed",
-        description: "it is now changed  test",
-        image: "image test",
-        categories: ["test1", "test2"],
-        size: "1 test",
-        color: "color test",
-        price: 1,
-      })
-      .expect(200);
+  describe("PUT/ updateProduct", () => {
+    it("should update info with given valid id", async () => {
+      const product = new Product(productTest);
+      await product.save();
+      const res = await request(server)
+        .put("/api/product/" + product._id)
+        .send({
+          title: "changed changed changed",
+          description: "it is now changed  test",
+          image: "image test",
+          categories: ["test1", "test2"],
+          size: "1 test",
+          color: "color test",
+          price: 1,
+        })
+        .expect(200);
 
-    const productTitle = await Product.findById(product._id);
+      const productTitle = await Product.findById(product._id);
 
-    expect(productTitle.title).toBe("changed changed changed");
+      expect(productTitle.title).toBe("changed changed changed");
+    });
+    it("should return 404 when id is invalid", async () => {
+      const res = await request(server)
+        .put("/api/product/1")
+        .send({
+          title: "changed changed changed",
+          description: "it is now changed  test",
+          image: "image test",
+          categories: ["test1", "test2"],
+          size: "1 test",
+          color: "color test",
+          price: 1,
+        })
+        .expect(404);
+    });
   });
-  it("should return 404 when id is invalid", async () => {
-    const res = await request(server)
-      .put("/api/product/1")
-      .send({
-        title: "changed changed changed",
-        description: "it is now changed  test",
-        image: "image test",
-        categories: ["test1", "test2"],
-        size: "1 test",
-        color: "color test",
-        price: 1,
-      })
-      .expect(404);
-  });
-});
-describe("delete/ deleteProduct ", () => {
-  it("should delete product with given valid id ", async () => {
-    const product = new Product(productTest);
-    await product.save();
-    await request(server)
-      .delete("/api/product/" + product._id)
-      .expect(200)
-      .then((e) => {
-        expect(e.body).toBe("product has been deleted...");
-      });
-  });
-  it("should return 404 if id was invalid ", async () => {
-    const res = await request(server).delete("/api/product/1").expect(404);
+  describe("DELETE/ deleteProduct ", () => {
+    it("should delete product with given valid id ", async () => {
+      const product = new Product(productTest);
+      await product.save();
+      await request(server)
+        .delete("/api/product/" + product._id)
+        .expect(200)
+        .then((e) => {
+          expect(e.body).toBe("product has been deleted...");
+        });
+    });
+    it("should return 404 if id was invalid ", async () => {
+      const res = await request(server).delete("/api/product/1").expect(404);
+    });
   });
 });
---
+++ server/testing/validaters.test.js
@@ -0,0 +1,177 @@
+const {
+  validateCreateProduct,
+  validateCreateOrder,
+  validateCreateCart,
+  validateRegister,
+  validateLogin,
+} = require("../modules/validaters");
+
+describe("validaters test", () => {
+  describe("validate Create Product ", () => {
+    const productTestCorrectly = {
+      title: "test product test",
+      description: "this is just test test product",
+      image: "image test",
+      categories: ["test1", "test2"],
+      size: "1 test",
+      color: "color test",
+      price: 1,
+    };
+    it("should pass if entered all info correctly", () => {
+      expect(validateCreateProduct(productTestCorrectly)).toStrictEqual({
+        value: productTestCorrectly,
+      });
+    });
+    it("should throw an error if there is missing info ", () => {
+      expect(
+        validateCreateProduct({
+          title: "test product test",
+          description: "this is just test test product",
+        })
+      ).toHaveProperty("error");
+    });
+    it("should throw an error if entered any invalid value", () => {
+      productTestCorrectly.title = 1;
+      expect(validateCreateProduct(productTestCorrectly)).toHaveProperty(
+        "error"
+      );
+      productTestCorrectly.title = "test product test";
+      productTestCorrectly.description = "hi";
+      expect(validateCreateProduct(productTestCorrectly)).toHaveProperty(
+        "error"
+      );
+    });
+  });
+  describe("validate Create Order ", () => {
+    const orderTestCorrectly = {
+      userId: "test userId test",
+      products: ["test1", "test2"],
+      amount: 1,
+      address: { test1: "test1" },
+      status: "1 test",
+    };
+    it("should pass if entered all info correctly", () => {
+      expect(validateCreateOrder(orderTestCorrectly)).toStrictEqual({
+        value: orderTestCorrectly,
+      });
+    });
+    it("should throw an error if there is missing info ", () => {
+      expect(
+        validateCreateOrder({
+          amount: 1,
+          address: { test1: "test1" },
+        })
+      ).toHaveProperty("error");
+    });
+    it("should throw an error if entered any invalid value", () => {
+      orderTestCorrectly.products = 1;
+      expect(validateCreateOrder(orderTestCorrectly)).toHaveProperty("error");
+      orderTestCorrectly.products = ["test1", "test2"];
+      orderTestCorrectly.address = "hi";
+      expect(validateCreateOrder(orderTestCorrectly)).toHaveProperty("error");
+    });
+  });
+  describe("validate Create Cart", () => {
+    const cartTestCorrectly = {
+      userId: "userId test",
+      products: ["product1", "product2"],
+    };
+    it("should pass if entered all info correctly", () => {
+      expect(validateCreateCart(cartTestCorrectly)).toStrictEqual({
+        value: cartTestCorrectly,
+      });
+    });
+    it("should throw an error if there is missing info ", () => {
+      expect(
+        validateCreateCart({ products: ["product1", "product2"] })
+      ).toHaveProperty("error");
+    });
+    it("should throw an error if entered any invalid value ", () => {
+      cartTestCorrectly.userId = 1;
+      expect(validateCreateCart(cartTestCorrectly)).toHaveProperty("error");
+      (cartTestCorrectly.userId = "user test"),
+        (cartTestCorrectly.products = { product: "product" });
+      expect(validateCreateCart(cartTestCorrectly)).toHaveProperty("error");
+    });
+  });
+  describe("validate Register", () => {
+    const newRegistering = {
+      username: "usernameTestrequired",
+      email: "test@required.com",
+      password: "passwordTest@req",
+    };
+    it("should pass if entered all info correctly", () => {
+      expect(validateRegister(newRegistering)).toStrictEqual({
+        value: newRegistering,
+      });
+    });
+    it("should throw an error if there is missing info ", () => {
+      expect(
+        validateRegister({
+          username: "usernameTestrequired",
+        })
+      ).toHaveProperty("error");
+      expect(
+        validateRegister({
+          email: "test@required.com",
+        })
+      ).toHaveProperty("error");
+      expect(
+        validateRegister({
+          password: "test@required.com",
+        })
+      ).toHaveProperty("error");
+
+      expect(
+        validateRegister({
+          username: "usernameTestrequired",
+          email: "test@required.com",
+        })
+      ).toHaveProperty("error");
+      expect(
+        validateRegister({
+          username: "usernameTestrequired",
+          password: "passwordTest@req",
+        })
+      ).toHaveProperty("error");
+      expect(
+        validateRegister({
+          email: "test@required.com",
+          password: "passwordTest@req",
+        })
+      ).toHaveProperty("error");
+    });
+    it("should throw an error if entered any invalid value ", () => {
+      (newRegistering.username = "a"),
+        expect(validateRegister(newRegistering)).toHaveProperty("error");
+      newRegistering.username = "usernameTestrequired";
+      newRegistering.email = "invalid";
+      expect(validateRegister(newRegistering)).toHaveProperty("error");
+      newRegistering.email = "test@required.com";
+      newRegistering.password = "123";
+      expect(validateRegister(newRegistering)).toHaveProperty("error");
+    });
+  });
+  describe("validate Login", () => {
+    const login = {
+      email: "test@test.com",
+      password: "testRequirePass",
+    };
+    it("should pass if entered all info correctly", () => {
+      expect(validateLogin(login)).toStrictEqual({ value: login });
+    });
+    it("should throw an error if there is missing info ", () => {
+      expect(validateLogin({ email: "test@test.com" })).toHaveProperty("error");
+      expect(validateLogin({ password: "testRequirePass" })).toHaveProperty(
+        "error"
+      );
+    });
+    it("should throw an error if entered any invalid value ", () => {
+      login.email = "test";
+      expect(validateLogin(login)).toHaveProperty("error");
+      login.email = "test@test.com";
+      login.password = "123";
+      expect(validateLogin(login)).toHaveProperty("error");
+    });
+  });
+});
