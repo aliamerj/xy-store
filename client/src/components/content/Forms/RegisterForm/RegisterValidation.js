1d4b1f5b2b2387eecdeb3d2a37d08cbada28caf6
joeamerson <aliamer19ali@gmail.com>
2022-05-23 17:31:43 +0300

    make form

---
+++ client/src/components/content/Forms/RegisterForm/RegisterValidation.js
@@ -0,0 +1,34 @@
+import * as yup from "yup";
+const registerSchema = yup
+  .object({
+    firstName: yup
+      .string()
+      .matches(/^([^0-9]*)$/, "First name should not contain a number")
+      .min(3, "First name is so short")
+      .max(10, "First name is so long")
+      .required("First name  is a required field"),
+    lastName: yup
+      .string()
+      .matches(/^([^0-9]*)$/, "Last name should not contain a number")
+      .min(3, "Last name is so short")
+      .max(10, "Last name is so long")
+      .required("Last name  is a required field"),
+    email: yup.string().email().required(),
+    password: yup
+      .string()
+      .min(5, "Password so week")
+      .max(255, "password so long")
+      .required(),
+    RePassword: yup
+      .string()
+      .required("Please repeat your password")
+      .oneOf([yup.ref("password")], "Your passwords do not match."),
+    country: yup.string().required(),
+    city: yup.string().required(),
+    address1: yup.string().min(10).max(50),
+    username: yup.string().min(5).max(20),
+    zip: yup.number().typeError("zip must be a number"),
+  })
+  .required();
+
+export default registerSchema;
